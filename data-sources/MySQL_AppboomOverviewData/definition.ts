4dde17af0881d53dcb787f2f4fbe1648d41bf9cc
Quang Hoang <quang@smartmove.vn>
2025-07-21 14:17:08 +0700

    feat: Finalize moving overview to reports submodule

--- data-sources/MySQL_AppboomOverviewData/definition.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  source: 'mysql',
-  queryFile: 'query.sql',
-  dimensions: ['date', 'app_id', 'app_name', 'platform'],
-  metrics: ['revenue', 'cost', 'downloads', 'custom_costs'],
-}; 
